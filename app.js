9132d45a3d39019e535e1f9fed31a2eb13f80030
Veeresh Digasangi <veeresh.digasangi@mutualmobile.com>
2023-01-05 22:36:20 +0530

    feat: parcel is beast

--- app.js
+++ app.js
@@ -11,3 +11,24 @@ const container = React.createElement(
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
+
+/**
+ *  PARCEL FEATURES
+HMR (Hot Module Replacement)
+FWA (File Watcher Algorithm)
+Bundling
+Minify
+Cleaning code
+Dev and Production Build
+Super fast Build Algorithm
+Image Optimization
+Caching while development
+Compression
+Compatible with older version of browser
+HTTPS on dev
+Dynamic Port mapping
+Consistent Hashing Algorithms (To do all bundling)
+Zero Config (Other bundlers have much configuration)
+Tree Shaking - Remove unwanted
+ * 
+ */
