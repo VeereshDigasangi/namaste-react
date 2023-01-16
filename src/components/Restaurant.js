4b18a991ad540d08412f901ebcce317f42c70e8e
Veeresh Digasangi <veeresh.digasangi@mutualmobile.com>
2023-01-16 20:26:09 +0530

    feature:finding paths

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -2,10 +2,10 @@ import { CDN_URL } from "../common/constants";
 export default RestrauntCard = (props) => {
   return (
     <div className="card">
-      <img src={`${CDN_URL}${props.cloudinaryImageId}`} />
-      <h2>{props.name}</h2>
-      <h3>{props.cuisines.join()}</h3>
-      <h4>{props.rating}</h4>
+      <img src={`${CDN_URL}${props?.cloudinaryImageId}`} />
+      <h2>{props?.name}</h2>
+      <h3>{props?.cuisines?.join()}</h3>
+      <h4>{props?.rating}</h4>
     </div>
   );
 };
